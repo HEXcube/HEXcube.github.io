bab847f0a76452f5b50a7b8ca0a5d45fed8ea232
HEXcube <hexcubed@gmail.com>
2019-10-20 01:55:01 +0530

    Add script to autoupdate copyright year
    - Add jQuery via CDN as dependency for copyright year script
    - Restructure css and image folders

---
+++ assets/js/copyright-year-update.js
@@ -0,0 +1,11 @@
+/*
+  Autoupdate Copyright year
+
+  Derived from:
+  http://updateyourfooter.com
+  https://stackoverflow.com/questions/1358810/how-do-i-change-the-text-of-a-span-element-in-javascript#1358815
+*/
+
+$(function() {
+  document.getElementById("copyright-year").textContent = (new Date().getFullYear())
+})
